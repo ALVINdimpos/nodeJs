f5e25129df71bd4f6315fdca6a5e2bffbeb2ed77
ALVINdimpos <alvindimpos@gmail.com>
2023-03-09 16:00:12 +0200

    ft(FIX):i have fixed swagger issue

--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -5,7 +5,7 @@ const blogController = require("../controllers/blogController");
 router.get("/blogs", blogController.getAllBlogs);
 router.get("/blog/:id", blogController.getBlogById);
 router.post("/blog/create", blogController.createBlog);
-router.put("//blog/update/:id", blogController.updateBlog);
+router.put("/blog/update/:id", blogController.updateBlog);
 router.delete("/blog/delete/:id", blogController.deleteBlog);
 
 module.exports = router;
