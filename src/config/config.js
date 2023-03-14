e9c956bbb7bd558e40765ad270f2fd4fdd735783
ALVINdimpos <alvindimpos@gmail.com>
2023-03-14 14:31:47 +0200

    Add environment variables to config file
    This commit adds environment variables for port, database URL, and JWT secret to the config file. The variables are imported and used in the main server file for establishing a database connection and creating a JWT token.

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
+import crypto from 'crypto';
 
 export const port = process.env.PORT;
 export const database = process.env.DATABASE;
-export const secret = process.env.JWT_SECRET
+export const secret = crypto.randomBytes(32).toString('hex');;
