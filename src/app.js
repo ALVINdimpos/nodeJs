e18d8ce0b935e6b384202bf9560d9e73cb7a15fe
ALVINdimpos <alvindimpos@gmail.com>
2023-03-15 11:58:31 +0200

    feat: Add edit and update functionality for blog posts
    This commit adds the ability for users to edit and update their blog posts. The editBlog function is called when the edit button is clicked, which opens a form with the current blog post information pre-populated. When the form is submitted, the updateBlog function is called to send a PUT request to the server with the updated blog post information. If the update is successful, the user is given a success message and the blog post list is re-rendered with the updated post.

--- src/app.js
+++ src/app.js
@@ -33,6 +33,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Enable CORS
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // Define routes
 app.use("/api", blogRoutes);
 app.use("/api", userRoutes);
