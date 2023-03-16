f82c27a242946fb6f6318303eb5f5f8bc7925a2f
ALVINdimpos <alvindimpos@gmail.com>
2023-03-16 11:47:34 +0200

    feat: Add edit and update functionality for blog posts
    This commit adds the ability for users to edit and update their blog posts. The editBlog function is called when the edit button is clicked, which opens a form with the current blog post information pre-populated. When the form is submitted, the updateBlog function is called to send a PUT request to the server with the updated blog post information. If the update is successful, the user is given a success message and the blog post list is re-rendered with the updated post.

--- src/app.js
+++ src/app.js
@@ -18,6 +18,12 @@ const app = express();
 // Enable CORS
 app.use(cors());
 mongoose.set('strictQuery', false);
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 // Connect to the database
 mongoose.connect(database, {
